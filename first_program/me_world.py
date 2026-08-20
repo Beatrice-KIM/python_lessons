a81b4a12265bcf6c2821f439f3b7b0e274cdb765
Stephano  Siame <145379768+stephanosiame@users.noreply.github.com>
2026-08-20 12:05:17 +0300

    add comment for  good work
    Added comments to clarify the purpose of each print statement.

--- first_program/me_world.py
+++ first_program/me_world.py
@@ -1,7 +1,9 @@
-print("my name is Tricey")
-print("I study at Ardhi university")
-print("My former advance school was Namwai high school")
-print("MY privious o level education finished since 2022 at Don bosco sec")
-print("I decided to learning computer sience ")
-print("Since i wish to be a goog software developer")
-print("That a short story about Tricey")
+print("my name is Tricey") #good
+print("I study at Ardhi university")#good
+print("My former advance school was Namwai high school")#good
+print("MY privious o level education finished since 2022 at Don bosco sec")#good
+print("I decided to learning computer sience ")#good
+print("Since i wish to be a goog software developer")#good
+print("That a short story about Tricey")#good
+
+#excellent Tricey you doing good work so far
